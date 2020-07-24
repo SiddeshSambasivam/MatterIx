04e5f553c80dbeea5e62eaca0f9371d1420bf5f7
siddesh <plutocrat45@gmail.com>
2020-07-24 20:54:49 +0800

    added registry for activation functions

--- modules/activations.py
+++ modules/activations.py
@@ -1,20 +1,25 @@
 import numpy as np
 import typing
 import math
+from registry import register_activation
 
-
+@register_activation
 def sigmoid(x):
     return 1/(1+np.exp(-x))
 
+@register_activation
 def tanh(x):
     return (np.exp(x)-np.exp(-x))/(np.exp(x)+np.exp(-x))
 
+@register_activation
 def relu(x):
     return np.maximum(x,0)
 
+@register_activation
 def softmax(x):
     return np.exp(x)/np.sum(np.exp(x))
 
+@register_activation
 def lrelu(x, slope=0.01):
     y1 = (x>=0) * x
     y2 = (x<0)*slope*x
