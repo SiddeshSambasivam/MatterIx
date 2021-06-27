6efe2c27c641437b735b4019b5953d30fd72e59c
Siddesh Sambasivam <plutocrat45@gmail.com>
2021-06-27 19:48:27 +0530

    change version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="MatterIx",
-    version="2.0.0",
+    version="0.1.0",
     author="Siddesh Sambasivam Suseela",
     author_email="plutocrat45@gmail.com",
     description="Just another deep learning framework",
