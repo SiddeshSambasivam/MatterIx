0eb95f4ec7f4b0ebe40f9cd67778fa0036d39d03
Siddesh Sambasivam <plutocrat45@gmail.com>
2021-06-30 10:46:02 +0530

    add normalization

--- matterix/loss.py
+++ matterix/loss.py
@@ -3,10 +3,11 @@ from .tensor import Tensor
 # TODO: MAE, Binary cross-entropy, Categorical cross-entropy, kullback leibler divergence loss
 
 
-def MSE(y_train: Tensor, y_pred: Tensor) -> Tensor:
+def MSE(y_train: Tensor, y_pred: Tensor, norm: bool = True) -> Tensor:
 
     diff = y_train - y_pred
-    loss = (diff * diff).sum() * (1.0 / diff.numel())
+
+    loss = (diff * diff).sum() * (1.0 / diff.shape[0]) if norm else (diff * diff).sum()
 
     return loss
 
@@ -14,7 +15,7 @@ def MSE(y_train: Tensor, y_pred: Tensor) -> Tensor:
 def RMSE(y_train: Tensor, y_pred: Tensor) -> Tensor:
 
     diff = y_train - y_pred
-    mse = ((diff * diff).sum()) * (1.0 / diff.numel())
+    mse = ((diff * diff).sum()) * (1.0 / diff.shape[0])
     rmse = mse ** (1.0 / 2.0)
 
     return rmse
