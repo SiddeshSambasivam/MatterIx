c5fb1e81d18af97551e1c8cde6e06fd433c2c86a
Siddesh Sambasivam <plutocrat45@gmail.com>
2021-07-04 20:23:44 +0530

    add momentum to SGD

--- matterix/optim.py
+++ matterix/optim.py
@@ -1,7 +1,9 @@
 # TODO: Adam, RMSProp
+from collections import defaultdict
 
 
 class SGD:
+    # TODO: Momentum, mini-batching
     """
     Runs stochastic gradient descent
 
@@ -18,18 +20,28 @@ class SGD:
         Learning rate. Size of each gradient step
     """
 
-    def __init__(self, parameters: dict, lr: float = 0.001) -> None:
+    def __init__(
+        self, parameters: dict, lr: float = 0.001, momentum: float = 0.9
+    ) -> None:
         self.params = parameters
+        self.momentum = momentum
         self.lr = lr
+
+        self.momentum_dict = {param: 0.0 for param, _ in self.params.items()}
 
     def step(self) -> None:
         """Updates the parameters of the model"""
 
-        for k, v in self.params.items():
-            v -= v.grad * self.lr
-            self.params[k] = v
+        for param, weight in self.params.items():
+
+            v = (self.momentum * self.momentum_dict[param]) + (weight.grad * self.lr)
+            weight -= v
+
+            self.params[param] = weight
+            self.momentum_dict[param] = v
 
     def zero_grad(self) -> None:
         """Sets the gradients of all the parameters to zero"""
+
         for _, param in self.params.items():
             param.zero_grad()
